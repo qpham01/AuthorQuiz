f570a6290e01cac7c351b80cf35731a89f927664
= <quoc_pham@msn.com>
2018-12-16 11:57:24 -0600

    Added test for initial background color.

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import AuthorQuiz from './AuthorQuiz';
 import Enzyme, {mount, shalow, render} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16'
+import { wrap } from 'module';
 
 Enzyme.configure({adapter: new Adapter()});
 
@@ -28,6 +29,16 @@ describe("Author Quiz", () =>
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={()=>{}} />, div);
-    ReactDOM.unmountComponentAtNode(div);
+  }),
+
+  describe("When no answer has been selected", ()=>{
+    let wrapper;
+    beforeAll(()=>{
+      wrapper = mount(<AuthorQuiz {...state} onAnswerSelected={() => {}}/>);
+    });
+
+    it("should have no background color", () =>{
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor.toBe(''));
+    })
   })
 );
